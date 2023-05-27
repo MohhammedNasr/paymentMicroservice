aac7cdc80f9025e60c6bb38aae1ac3df03cb86d9
Habiba Elganzoury <habiba2109942@miuegypt.edu.eg>
2023-05-27 14:37:02 +0300

    delete/save/update functions added to controller

--- payment/src/main/java/com/project/payment/Controllers/paymentController.java
+++ payment/src/main/java/com/project/payment/Controllers/paymentController.java
@@ -7,8 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.project.payment.models.pay;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/library/Pay")
@@ -30,10 +35,30 @@ public class paymentController {
 
     }
 
+    @GetMapping("{payId}")
+    public ResponseEntity getPaymentById(@PathVariable(name = "payId") Long payId) {
+        pay payment = this.paymentRepository.findById(payId).orElse(null);
+        return new ResponseEntity<>(payment, HttpStatus.OK);
+
+    }
+
     @PostMapping("")
     public ResponseEntity savePay(@RequestBody pay pay) {
         pay res = paymentRepository.save(pay);
         return new ResponseEntity(res, HttpStatus.CREATED);
+    }
+
+    @PutMapping("{payId}")
+    public ResponseEntity updatePayment(@PathVariable(name = "payId") Long id, @RequestBody pay pay) {
+        pay.setId(id);
+        pay res = paymentRepository.save(pay);
+        return new ResponseEntity(res, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("{payId}")
+    public ResponseEntity deletePayment(@PathVariable(name = "payId") Long id) {
+        this.paymentRepository.deleteById(id);
+        return new ResponseEntity(HttpStatus.ACCEPTED);
     }
 
 }
