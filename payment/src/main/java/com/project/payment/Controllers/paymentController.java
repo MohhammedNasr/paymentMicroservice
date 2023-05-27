efdd3d752efc4a1226a77e23d1f48ba6ed7c8f94
Habiba Elganzoury <habiba2109942@miuegypt.edu.eg>
2023-05-27 14:05:27 +0300

    updates

--- payment/src/main/java/com/project/payment/Controllers/paymentController.java
+++ payment/src/main/java/com/project/payment/Controllers/paymentController.java
@@ -1,39 +1,39 @@
 package com.project.payment.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.project.payment.models.pay;
 
-@Controller
-@RequestMapping("/library")
+@RestController
+@RequestMapping("/library/Pay")
 public class paymentController {
     @Autowired
     private com.project.payment.Repositories.paymentRepository paymentRepository;
 
-    @GetMapping("pay")
-    public ModelAndView showPaymentForm(Model model) {
-        ModelAndView mav = new ModelAndView("Pay.html");
-        return mav;
+    @GetMapping("")
+    public ResponseEntity getAllPayments() {
+        List<pay> payments = this.paymentRepository.findAll();
+        return new ResponseEntity<>(payments, HttpStatus.OK);
 
     }
 
-    @PostMapping("/library/save-payment")
-    public String savePay(@ModelAttribute pay pay, Model model) {
-        paymentRepository.save(pay);
-        if (pay.getPaymentMethod().equals("cash-on-delivery")) {
-            return "paymentConfirmation";
-        } else if (pay.getPaymentMethod().equals("payment-via-visa")) {
-            return "payVisa";
-        }
-        return "redirect:/";
+    @PostMapping("")
+    public ResponseEntity savePay(@RequestBody pay pay) {
+        pay res = paymentRepository.save(pay);
+        return new ResponseEntity(res, HttpStatus.CREATED);
     }
 
 }
